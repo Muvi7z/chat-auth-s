2de51c9cfffac815678b8970b86f1d5d74762a3c
Ochir.zha <ochir-zhamsuev@yandex.ru>
2025-03-26 10:57:57 +0800

    add tracing 1

---
+++ internal/interceptor/tracing.go
@@ -0,0 +1,5 @@
+package interceptor
+
+//func SeverTracingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+//	span, ctx := StartSpan
+//}
